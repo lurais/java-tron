aa8b1f36b2a926513cccd7a7d4abf869453da1e8
morgan <morgan.p@qq.com>
2022-12-18 21:15:10 +0800

    optimizeDefaultFlushSize

--- plugins/src/main/java/org/tron/plugins/DbExpand.java
+++ plugins/src/main/java/org/tron/plugins/DbExpand.java
@@ -1,5 +1,6 @@
 package org.tron.plugins;
 
+import com.google.common.collect.Lists;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.typesafe.config.Config;
@@ -41,7 +42,7 @@ public class DbExpand implements Callable<Integer> {
     private static final String SECOND_PATH_CONFIG_KEY = "secondPath";
     private static final String RATE_CONFIG_KEY = "rate";
     private static final String OP_CONFIG_KEY = "op";// 0直接膨胀 1混合 2先冷后热
-    private static final int BATCH = 4096;
+    private static final int BATCH = 1024;
     private Random random = new Random(System.currentTimeMillis());
     public static final byte ADD_PRE_FIX_BYTE_MAINNET = (byte) 0x41;
     private static Set<String> structureNames = new HashSet<>();
@@ -116,7 +117,7 @@ public class DbExpand implements Callable<Integer> {
         List<byte[]> keys = new ArrayList<>(BATCH);
         List<byte[]> values = new ArrayList<>(BATCH);
         try (DBIterator levelIterator = originDb.iterator(
-            new org.iq80.leveldb.ReadOptions().fillCache(false))) {
+                new org.iq80.leveldb.ReadOptions().fillCache(false))) {
             JniDBFactory.pushMemoryPool(2048 * 2048);
             levelIterator.seekToFirst();
 
@@ -129,7 +130,7 @@ public class DbExpand implements Callable<Integer> {
                         batchInsert(targetDb, keys, values);
                     } catch (Exception e) {
                         spec.commandLine().getErr().println(String.format("Batch insert kv error %s."
-                            , e.getMessage()));
+                                , e.getMessage()));
                     }
                 }
             }
@@ -139,12 +140,12 @@ public class DbExpand implements Callable<Integer> {
                     batchInsert(targetDb, keys, values);
                 } catch (Exception e) {
                     spec.commandLine().getErr().println(String.format("Batch insert kv error %s."
-                        , e.getMessage()));
+                            , e.getMessage()));
                 }
             }
         } catch (Exception e) {
             spec.commandLine().getErr().println(String.format("Merge error %s."
-                , e.getMessage()));
+                    , e.getMessage()));
         } finally {
             spec.commandLine().getOut().println("Merge db done");
         }
@@ -157,13 +158,24 @@ public class DbExpand implements Callable<Integer> {
         }
         //先冷后热（扩展冷库，merge）
         //混合，直接新库 SECOND_PATH_CONFIG_KEY
+        //导出新库
         return openLevelDb(Paths.get(database.toString(),dbPath,c.getString(NAME_CONFIG_KEY)+"_second"));
     }
 
     private void doExpand(DB levelDb,DB secondDb, String name, int rate,int op) {
         long expandKeyCount = 0;
+        List<byte[]> statKeys = new ArrayList<>(100);
+        Double statCount = 0.0;
+        double statAllTime = 0L;
+        long iterCount = 0L;
+        Boolean hasPrintStat = Boolean.FALSE;
         List<byte[]> keys = new ArrayList<>(BATCH);
         List<byte[]> values = new ArrayList<>(BATCH);
+        Set<String> levelPrintSet = new HashSet<>();
+        String maxLevel = "--------------------------------------------------";
+
+        double currentMax = 0.0;
+
         try (DBIterator levelIterator = levelDb.iterator(
                 new org.iq80.leveldb.ReadOptions().fillCache(false))) {
             JniDBFactory.pushMemoryPool(2048 * 2048);
@@ -171,17 +183,46 @@ public class DbExpand implements Callable<Integer> {
 
             while (levelIterator.hasNext()) {
                 Map.Entry<byte[], byte[]> entry = levelIterator.next();
-                addShuffleKv(op,name,entry, rate, keys, values);
+                iterCount+=1;
+                if(statCount>=1000.0 && !hasPrintStat){
+                    String currentLevel = levelDb.getProperty("leveldb.stats");
+                    maxLevel=parseLevel(currentLevel.split("\n")[currentLevel.split("\n").length-1]).trim();
+                    statAllTime = statQueryTime(levelDb,statKeys);
+                    spec.commandLine().getOut().println(name + " leveldb stat ,currentLevel="+maxLevel+",avg get:"+statAllTime/statCount+", statCount="+statCount+", iterCount="+iterCount);
+                    hasPrintStat=Boolean.TRUE;
+                }
+                addShuffleKv(op,name,entry, rate, keys, values,statCount);
+                for(byte[] key:keys) {
+                    if (statCount < 1000.0) {
+                        statKeys.add(key);
+                        statCount++;
+                    }
+                }
                 expandKeyCount += rate;
                 if (keys.size() >= BATCH) {
                     try {
+                        currentMax = statQueryTime(secondDb,keys)/keys.size() > currentMax? statQueryTime(secondDb,keys)/keys.size():currentMax;
+                        List<byte[]> tempKeys = copyList(keys);
                         batchInsert(op==0?levelDb:secondDb, keys, values);
+                        statQueryTime(secondDb,tempKeys);
+                        String currentLevel = secondDb.getProperty("leveldb.stats");
+                        maxLevel=parseLevel(currentLevel.split("\n")[currentLevel.split("\n").length-1]);
+                        if(!levelPrintSet.contains(maxLevel)){
+                            StringBuilder sb = new StringBuilder();
+                            levelPrintSet.stream().forEach(item->sb.append(item+","));
+                            statAllTime = statQueryTime(secondDb,statKeys);
+                            spec.commandLine().getOut().println(name + " leveldb level up,currentLevel="+maxLevel+",avg get:"+statAllTime/statCount+", statCount="+statCount+", iterCount="+iterCount+", set="+sb.toString());
+                            levelPrintSet.add(maxLevel);
+                        }
                     } catch (Exception e) {
                         spec.commandLine().getErr().println(String.format("Batch insert shuffled kv to %s error %s."
                                 , name, e.getStackTrace()));
                     }
                 }
             }
+
+            statAllTime = statQueryTime(secondDb,statKeys);
+            spec.commandLine().getOut().println(name + " leveldb level final,currentLevel="+maxLevel+",avg get:"+statAllTime/statCount+", statCount="+statCount+", iterCount="+iterCount);
 
             if (!keys.isEmpty()) {
                 try {
@@ -212,8 +253,48 @@ public class DbExpand implements Callable<Integer> {
         }
     }
 
+    private String parseLevel(String lastLine) {
+        if(lastLine==null||lastLine.length()<=0){
+            return "";
+        }
+        return lastLine.trim().charAt(0)+"";
+    }
+
+    private double statQueryTime(DB secondDb, List<byte[]> statKeys) {
+        double result = 0L;
+        for(byte[] key:statKeys){
+            long current = System.nanoTime();
+            byte[] cur = secondDb.get(key);
+            result+=(System.nanoTime() - current)/1000000.0;
+        }
+        return result;
+    }
+
+    private boolean levelUp(DB secondDb, String maxLevel) {
+        String currentLevel = secondDb.getProperty("leveldb.stats");
+        if(currentLevel==null||currentLevel.isEmpty()) return Boolean.FALSE;
+        if(parseLevel(currentLevel.split("\n")[currentLevel.split("\n").length-1]).trim().equals(maxLevel)){
+            return Boolean.FALSE;
+        }else{
+            return Boolean.TRUE;
+        }
+    }
+
     private void addShuffleKv(int op, String name, Map.Entry<byte[], byte[]> entry, int rate,
-                              List<byte[]> keys, List<byte[]> values) {
+                              List<byte[]> keys, List<byte[]> values,double statCount) {
+        if(op==3){
+            keys.add(entry.getKey());
+            values.add(entry.getValue());
+            if(statCount<1000){
+                for (int i = rate; i > 0; i--) {
+                    byte[] key = structureKey(name) ? generateStructure(name): shuffleBytes(entry.getKey());
+                    byte[] value = entry.getValue();
+                    keys.add(key);
+                    values.add(value);
+                }
+            }
+            return;
+        }
         for (int i = rate; i > 0; i--) {
             byte[] key = structureKey(name) ? generateStructure(name): shuffleBytes(entry.getKey());
             byte[] value = entry.getValue();
@@ -237,7 +318,7 @@ public class DbExpand implements Callable<Integer> {
     }
 
     private boolean structureKey(String name) {
-       return structureNames.contains(name);
+        return structureNames.contains(name);
     }
 
     public static byte[] generateAddress(int length) {
@@ -257,6 +338,16 @@ public class DbExpand implements Callable<Integer> {
         byte[] copyItem = new byte[item.length];
         System.arraycopy(item,0,copyItem,0,copyItem.length);
         return copyItem;
+    }
+
+    private List<byte[]> copyList(List<byte[]> keys){
+        List<byte[]> res = Lists.newArrayList();
+        for(byte[] key:keys){
+            byte[] copyItem = new byte[key.length];
+            System.arraycopy(key,0,copyItem,0,copyItem.length);
+            res.add(copyItem);
+        }
+        return res;
     }
 
     private void batchInsert(DB db, List<byte[]> keys, List<byte[]> values)
@@ -289,7 +380,7 @@ public class DbExpand implements Callable<Integer> {
         dbOptions.compressionType(CompressionType.SNAPPY);
         dbOptions.blockSize(4 * 1024);
         dbOptions.writeBufferSize(10 * 1024 * 1024);
-        dbOptions.cacheSize(10 * 1024 * 1024L);
+        dbOptions.cacheSize(0L);
         dbOptions.maxOpenFiles(1000);
         dbOptions.maxBatchSize(64_000);
         dbOptions.maxManifestSize(128);
